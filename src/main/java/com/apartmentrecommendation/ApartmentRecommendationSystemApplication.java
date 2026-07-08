bb7fded9cc649ce544197048c5bbc636b38f267c
Amol Bhandarkote <amolbhandarkote@gmail.com>
2026-07-08 11:37:21 +0530

    first commit

---
+++ src/main/java/com/apartmentrecommendation/ApartmentRecommendationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.apartmentrecommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApartmentRecommendationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApartmentRecommendationSystemApplication.class, args);
+	}
+
+}
